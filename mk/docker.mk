6ff23614060ab0ac02c3247f135f8be8002886cc
pancake <pancake@nopcode.org>
2016-11-30 15:47:37 +0100

    Drop the priv bit in stunnel

--- mk/docker.mk
+++ mk/docker.mk
@@ -1,14 +1,16 @@
+
 all:
 	docker build -t $(NAME) .
 
 run:
-	docker run --privileged -p $(PORT):$(PORT) -ti $(NAME)
+	docker run $(DOCKER_RFLAGS) -p $(PORT):$(PORT) -ti $(NAME)
 
 bg:
-	docker run --privileged -p $(PORT):$(PORT) -d $(NAME)
+	docker run $(DOCKER_RFLAGS) -p $(PORT):$(PORT) -d $(NAME)
 
 sh shell:
-	docker run --privileged --entrypoint /bin/sh -ti $(NAME)
+	docker run --entrypoint /bin/sh -ti $(NAME)
+	#docker run --privileged --entrypoint /bin/sh -ti $(NAME)
 
 clean:
 	docker rmi -f $(NAME)
